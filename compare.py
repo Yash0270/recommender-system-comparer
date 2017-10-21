c0925cc6bf8c599bd4b49bb67a8ccf589c774ac4
Kushal Agrawal <f2015289@hyderabad.bits-pilani.ac.in>
2017-10-21 21:41:40 +0530

    Add ratings data

---
+++ compare.py
@@ -0,0 +1,19 @@
+'''
+This script compares three different recommendation algorithms for generation
+of missing/expected movie ratings and displays the results to the user.
+
+Author: Kushal Agrawal
+Date of Completion: 30/09/2017
+
+The dataset containing the movie ratings was obtained from www.movielens.org
+and the following paper is responsible for its creation:
+
+F. Maxwell Harper and Joseph A. Konstan. 2015. The MovieLens Datasets: History
+and Context. ACM Transactions on Interactive Intelligent Systems (TiiS) 5, 4,
+Article 19 (December 2015), 19 pages. DOI=http://dx.doi.org/10.1145/2827872
+'''
+
+import numpy
+import scipy
+
+
