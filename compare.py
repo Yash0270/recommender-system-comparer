8bbe01eefec6396b687ed3322d04b50fd1a933a1
Kushal Agrawal <f2015289@hyderabad.bits-pilani.ac.in>
2017-11-12 18:28:30 +0530

    Add Collaborative Filtering and SVD

--- compare.py
+++ compare.py
@@ -13,8 +13,10 @@ and Context. ACM Transactions on Interactive Intelligent Systems (TiiS) 5, 4,
 Article 19 (December 2015), 19 pages. DOI=http://dx.doi.org/10.1145/2827872
 '''
 
+import os.path
+import math
+import heapq
 import numpy
-import scipy.sparse
 import tkinter as tk
 
 
@@ -65,7 +67,7 @@ class RecommenderSystems(tk.Frame):
         # The Message Box to display the output
         self.messageBox = tk.Message(self, anchor=tk.NW, padx=25, pady=25, width=500, justify=tk.LEFT, relief=tk.RIDGE,
                                      bd=3, textvariable=self.msg)
-        self.messageBox.grid(row=0, column=0, rowspan=8, columnspan=8, padx=20, pady=25,
+        self.messageBox.grid(row=0, column=0, rowspan=2, columnspan=8, padx=20, pady=25,
                              sticky=tk.N + tk.E + tk.S + tk.W)
 
     def setMessage(self, message):
@@ -73,34 +75,417 @@ class RecommenderSystems(tk.Frame):
         self.msg.set(message)
 
 
-    #############################################
-    ########### Data Loading Function ###########
-    #############################################
+
+    ###############################################
+    ########### Data Loading Function #############
+    ###############################################
 
     def loadData(self):
-        ''' Load the ratings data. '''
+        ''' Load the ratings data and run the algorithms. '''
 
         # File with the ratings data
-        self.ratings_file = open('./ratings/ratings.dat', 'r')
+        ratings_file = open('./ratings/ratings.dat', 'r')
 
         # Dictionary of keys format sparse matrix
-        self.ratings_matrix = scipy.sparse.dok_matrix((4000, 6100))
+        self.ratings_matrix = numpy.zeros((3952, 6040))
 
-        for rating in self.ratings_file:
+        for rating in ratings_file:
             data = rating.strip().split('::')
             self.ratings_matrix[int(data[1]) - 1, int(data[0]) - 1] = float(data[2])
 
-        self.ratings_file.close()
+        ratings_file.close()
 
-        self.ratings_matrix = self.ratings_matrix.asformat('coo').asformat('csr')
+        self.common()
+
+        # self.colab()
+
+        self.svd()
+
+        # self.cur()
 
         self.setMessage('Loading Complete.')
 
-    #############################################
+    ###############################################
 
 
+    ###############################################
+    ######## Common Processing Function ###########
+    ###############################################
+
+    def common(self):
+        ''' Perform calculations common to various algorithms '''
+
+        self.transpose_ratings_matrix = numpy.transpose(self.ratings_matrix)
+
+        self.total_mean = self.ratings_matrix.mean()
+
+        self.movie_sums = self.ratings_matrix.sum(1)
+
+        self.movie_non_zeros = numpy.count_nonzero(self.ratings_matrix, 1)
+
+        self.movie_means = numpy.zeros(self.movie_sums.shape)
+
+        for i in range(self.movie_sums.size):
+            if self.movie_non_zeros[i] != 0:
+                self.movie_means[i] = self.movie_sums[i] / self.movie_non_zeros[i]
+
+        self.user_sums = self.transpose_ratings_matrix.sum(1)
+
+        self.user_non_zeros = numpy.count_nonzero(self.transpose_ratings_matrix, 1)
+
+        self.user_means = numpy.zeros(self.user_sums.shape)
+
+        for i in range(self.user_sums.size):
+            if self.user_non_zeros[i] != 0:
+                self.user_means[i] = self.user_sums[i] / self.user_non_zeros[i]
+
+        self.training_ratings_matrix = numpy.zeros(self.ratings_matrix.shape)
+
+        for i in range(0, 400):
+            for j in range(400, self.ratings_matrix.shape[1]):
+                self.training_ratings_matrix[i][j] = self.ratings_matrix[i][j]
+
+        for i in range(400, self.ratings_matrix.shape[0]):
+            for j in range(0, self.ratings_matrix.shape[1]):
+                self.training_ratings_matrix[i][j] = self.ratings_matrix[i][j]
+
+        self.transpose_training_ratings_matrix = numpy.zeros(self.transpose_ratings_matrix.shape)
+
+        for i in range(0, 400):
+            for j in range(400, self.transpose_ratings_matrix.shape[1]):
+                self.transpose_training_ratings_matrix[i][j] = self.transpose_ratings_matrix[i][j]
+
+        for i in range(400, self.transpose_ratings_matrix.shape[0]):
+            for j in range(0, self.transpose_ratings_matrix.shape[1]):
+                self.transpose_training_ratings_matrix[i][j] = self.transpose_ratings_matrix[i][j]
+
+        self.training_total_mean = self.training_ratings_matrix.mean()
+
+        for i in range(self.ratings_matrix.shape[0]):
+            for j in range(self.ratings_matrix.shape[1]):
+                if self.training_ratings_matrix[i][j] != 0:
+                    self.training_ratings_matrix[i][j] -= self.training_total_mean
+                if self.transpose_training_ratings_matrix[j][i] != 0:
+                    self.transpose_training_ratings_matrix[j][i] -= self.training_total_mean
+
+    ###############################################
+
+    ###############################################
+    #### Preprocessing Collaborative Filtering ####
+    ###############################################
+
+    def preprocessColab(self):
+        ''' Preprocess the data for collaborative filtering. '''
+
+        magnitudes = numpy.zeros(self.movie_sums.shape)
+        norm_ratings_matrix = numpy.zeros(self.ratings_matrix.shape)
+        for i in range(self.ratings_matrix.shape[0]):
+            for j in range(self.ratings_matrix.shape[1]):
+                if self.ratings_matrix[i, j] != 0:
+                    norm_ratings_matrix[i, j] = (self.ratings_matrix[i, j] - self.movie_means[i])
+                    magnitudes[i] += norm_ratings_matrix[i, j] * norm_ratings_matrix[i, j]
+            magnitudes[i] = math.sqrt(magnitudes[i])
+            for j in range(self.ratings_matrix.shape[1]):
+                if magnitudes[i] != 0.0:
+                    norm_ratings_matrix[i, j] = norm_ratings_matrix[i, j] / magnitudes[i]
+
+        similarities = numpy.zeros((self.ratings_matrix.shape[0], self.ratings_matrix.shape[0]))
+        most_similar = numpy.zeros((self.ratings_matrix.shape[0], 100))
+
+        most_similar_file = open('./ratings/most_similar.dat', 'w')
+
+        for i in range(self.ratings_matrix.shape[0]):
+            for j in range(i + 1, norm_ratings_matrix.shape[0]):
+                for k in range(norm_ratings_matrix.shape[1]):
+                    similarities[i][j] += norm_ratings_matrix[i, k] * norm_ratings_matrix[j, k]
+                similarities[j][i] = similarities[i][j]
+            most_similar[i] = heapq.nlargest(100, range(len(similarities[i])), similarities[i].take)
+            for j in range(0, 100):
+                write_string = str(i) + '::' + str(j) + '::' + str(int(most_similar[i][j])) + '::' + str(
+                    similarities[i][int(most_similar[i][j])]) + '\n'
+                most_similar_file.write(write_string)
+
+        most_similar_file.close()
+
+    ###############################################
 
 
+    ###############################################
+    ########### Collaborative Filtering ###########
+    ###############################################
+
+    def colab(self):
+        ''' Run collaborative filtering algorithms. '''
+
+        if not os.path.exists('./ratings/most_similar.dat'):
+            self.preprocessColab()
+
+        most_similar_file = open('./ratings/most_similar.dat', 'r')
+
+        similarities = numpy.zeros((self.ratings_matrix.shape[0], self.ratings_matrix.shape[0]))
+        top_100 = {}
+
+        for line in most_similar_file:
+            data = line.strip().split('::')
+            i = int(data[0])
+            j = int(data[2])
+            s = data[3]
+            if i not in top_100:
+                top_100[i] = []
+            top_100[i].append(j)
+            similarities[i][j] = s
+
+        most_similar_file.close()
+
+        guesses = numpy.zeros((400, 400))
+        baseline_guesses = numpy.zeros((400, 400))
+
+        rmse = 0
+        baseline_rmse = 0
+        values_tested = 0
+
+        for i in range(400):
+            for j in range(400):
+                if self.ratings_matrix[i][j] != 0:
+                    values_tested += 1
+                    simsum = 0.0
+                    for k in top_100[i]:
+                        if self.ratings_matrix[k][j] != 0:
+                            guesses[i][j] += self.ratings_matrix[k][j] * similarities[i][k]
+                            baseline_score = self.total_mean + self.movie_means[k] + self.user_means[j]
+                            baseline_guesses[i][j] += (self.ratings_matrix[k][j] - baseline_score) * similarities[i][k]
+                            simsum += similarities[i][k]
+                    if simsum != 0.0:
+                        guesses[i][j] /= simsum
+                        baseline_guesses[i][j] /= simsum
+                    baseline_guesses[i][j] += self.total_mean + self.movie_means[i] + self.user_means[j]
+                    guess_error = guesses[i][j] - self.ratings_matrix[i][j]
+                    rmse += guess_error ** 2
+                    baseline_guess_error = baseline_guesses[i][j] - self.ratings_matrix[i][j]
+                    baseline_rmse += baseline_guess_error ** 2
+
+        rmse /= values_tested
+        rho = 1 - (6 / (values_tested ** 2 - 1)) * rmse
+        rmse = math.sqrt(rmse)
+        baseline_rmse /= values_tested
+        baseline_rho = 1 - (6 / (values_tested ** 2 - 1)) * baseline_rmse
+        baseline_rmse = math.sqrt(baseline_rmse)
+
+        print(rmse, ' ', baseline_rmse)
+
+        t_guesses = numpy.transpose(guesses)
+        t_baseline_guesses = numpy.transpose(baseline_guesses)
+
+        precision_at_10 = 0.0
+        baseline_precision_at_10 = 0.0
+
+        for i in range(400):
+            top_10 = heapq.nlargest(10, range(400), t_guesses[i].take)
+            for r in top_10:
+                if r >= 3.5:
+                    precision_at_10 += 1
+            top_10 = heapq.nlargest(10, range(400), t_baseline_guesses[i].take)
+            for r in top_10:
+                if r >= 3.5:
+                    baseline_precision_at_10 += 1
+
+        precision_at_10 /= 4000
+        baseline_precision_at_10 /= 4000
+
+        print(precision_at_10 * 100, '% ', baseline_precision_at_10 * 100, '%')
+
+        print(rho, ' ', baseline_rho)
+
+        print()
+
+    ###############################################
+
+
+    ###############################################
+    ######### Singular Value Decomposition ########
+    ###############################################
+
+    def svd_calc(self, A):
+        ''' Run SVD algorithms. '''
+
+        AAt = numpy.dot(A, numpy.transpose(A))
+
+        AtA = numpy.dot(numpy.transpose(A), A)
+
+        x1, y1 = numpy.linalg.eigh(AAt)
+        y1 = numpy.transpose(y1)
+        ar = numpy.argsort(x1)
+        ar = numpy.flipud(ar)
+        sx1 = numpy.copy(x1)
+        sy1 = numpy.copy(y1)
+        for i in range(len(ar)):
+            sx1[i] = x1[ar[i]]
+            sy1[i] = y1[ar[i]]
+        u = numpy.transpose(sy1)
+
+        x2, y2 = numpy.linalg.eigh(AtA)
+        y2 = numpy.transpose(y2)
+        ar = numpy.argsort(x2)
+        ar = numpy.flipud(ar)
+        sx2 = numpy.copy(x2)
+        sy2 = numpy.copy(y2)
+        for i in range(len(ar)):
+            sx2[i] = x2[ar[i]]
+            sy2[i] = y2[ar[i]]
+        v = numpy.transpose(sy2)
+        vt = numpy.transpose(v)
+
+        r = min(u.shape[1], vt.shape[0])
+        for i in range(r):
+            if sx1[i] <= 0:
+                r = i
+                break
+
+        sigma = numpy.zeros((r, r))
+        for i in range(r):
+            sigma[i][i] = sx1[i]
+        sigma = numpy.sqrt(sigma)
+
+        u = u[:, :r]
+        vt = vt[:r, :]
+
+        return u, sigma, vt
+
+    def svd(self):
+        ''' Error calculation and driver method for SVD. '''
+
+        u, sigma, vt = self.svd_calc(self.transpose_training_ratings_matrix)
+
+        guesses = numpy.dot(u, numpy.dot(sigma, vt))
+
+        for i in range(guesses.shape[0]):
+            for j in range(guesses.shape[1]):
+                guesses[i][j] += self.training_total_mean
+
+        rmse = 0
+        values_tested = 0
+
+        for i in range(400):
+            for j in range(400):
+                if self.transpose_ratings_matrix[i][j] != 0:
+                    values_tested += 1
+                    guess_error = guesses[i][j] - self.transpose_ratings_matrix[i][j]
+                    rmse += guess_error ** 2
+
+        rmse /= values_tested
+        rho = 1 - (6 / (values_tested ** 2 - 1)) * rmse
+        rmse = math.sqrt(rmse)
+
+        print(rmse)
+
+        t_guesses = numpy.transpose(guesses)
+        precision_at_10 = 0.0
+
+        for i in range(400):
+            top_10 = heapq.nlargest(10, range(400), t_guesses[i].take)
+            for r in top_10:
+                if r >= 3.5:
+                    precision_at_10 += 1
+
+        precision_at_10 /= 4000
+
+        print(precision_at_10 * 100, '%')
+
+        print(rho)
+
+        print()
+
+    ###############################################
+
+
+    ###############################################
+    ############## CUR Decomposition ##############
+    ###############################################
+
+    def cur(self):
+        ''' Run CUR Decomposition algorithms. '''
+
+        A = self.transpose_training_ratings_matrix
+
+        AAt = numpy.dot(A, numpy.transpose(A))
+
+        AtA = numpy.dot(numpy.transpose(A), A)
+
+        x1, y1 = numpy.linalg.eigh(AAt)
+        y1 = numpy.transpose(y1)
+        ar = numpy.argsort(x1)
+        ar = numpy.flipud(ar)
+        sx1 = numpy.copy(x1)
+        sy1 = numpy.copy(y1)
+        for i in range(len(ar)):
+            sx1[i] = x1[ar[i]]
+            sy1[i] = y1[ar[i]]
+        u = numpy.transpose(sy1)
+
+        x2, y2 = numpy.linalg.eigh(AtA)
+        y2 = numpy.transpose(y2)
+        ar = numpy.argsort(x2)
+        ar = numpy.flipud(ar)
+        sx2 = numpy.copy(x2)
+        sy2 = numpy.copy(y2)
+        for i in range(len(ar)):
+            sx2[i] = x2[ar[i]]
+            sy2[i] = y2[ar[i]]
+        v = numpy.transpose(sy2)
+        vt = numpy.transpose(v)
+
+        r = min(u.shape[1], vt.shape[0])
+        for i in range(r):
+            if sx1[i] <= 0:
+                r = i
+                break
+
+        sigma = numpy.zeros((r, r))
+        for i in range(r):
+            sigma[i][i] = sx1[i]
+        sigma = numpy.sqrt(sigma)
+
+        u = u[:, :r]
+        vt = vt[:r, :]
+        guesses = numpy.dot(u, numpy.dot(sigma, vt))
+        for i in range(guesses.shape[0]):
+            for j in range(guesses.shape[1]):
+                guesses[i][j] += self.training_total_mean
+
+        rmse = 0
+        values_tested = 0
+
+        for i in range(400):
+            for j in range(400):
+                if self.transpose_ratings_matrix[i][j] != 0:
+                    values_tested += 1
+                    guess_error = guesses[i][j] - self.transpose_ratings_matrix[i][j]
+                    rmse += guess_error ** 2
+
+        rmse /= values_tested
+        rho = 1 - (6 / (values_tested ** 2 - 1)) * rmse
+        rmse = math.sqrt(rmse)
+
+        print(rmse)
+
+        t_guesses = numpy.transpose(guesses)
+        precision_at_10 = 0.0
+
+        for i in range(400):
+            top_10 = heapq.nlargest(10, range(400), t_guesses[i].take)
+            for r in top_10:
+                if r >= 3.5:
+                    precision_at_10 += 1
+
+        precision_at_10 /= 4000
+
+        print(precision_at_10 * 100, '%')
+
+        print(rho)
+
+        print()
+
+    ###############################################
 
 ###############################################################################
 
